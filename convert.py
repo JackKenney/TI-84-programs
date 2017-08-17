65d3d61b2f706c2a64924253b2aab17a79b1a7d3
Jack Kenney <kenney.jnk@gmail.com>
2017-08-17 17:03:56 -0400

    cleaned convert.py file

--- convert.py
+++ convert.py
@@ -6,20 +6,17 @@
 # Which can be found at https://github.com/thenaterhood/basically-ti-basic
 import os
 from subprocess import call
-
 # Determine which files to convert and move
 names = []
 for root, dirs, files in os.walk('.'):
     for file in files:
         if file.endswith('.8Xp'):
             names.append(file)
-
 # Make sure the 8Xp directory has been created.
 if not os.path.isdir("8Xp"):
     call(['mkdir', '8Xp'])
 if not os.path.isdir("txt"):
     call(['mkdir', 'txt'])
-
 # Begin
 for name in names:
     txt = name[0:len(name)-4]+".txt"
@@ -31,7 +28,6 @@ for name in names:
     call(["mv", txt, "txt/"+txt])
 
 # Sources for python scripting:
-
 # https://docs.python.org/3/library/os.html#os.fwalk
 # https://stackoverflow.com/questions/1274506/how-can-i-create-a-list-of-files-in-the-current-directory-and-its-subdirectories
 # https://stackoverflow.com/questions/89228/calling-an-external-command-in-python
